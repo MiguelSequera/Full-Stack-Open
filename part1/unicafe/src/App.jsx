5f5491389d855d280f78413a255ee5ee16354ae9
Miguel Sequera <msequera197@gmail.com>
2024-11-01 15:28:34 -0400

    Exercise 1.7 Unicafe step 2: Expand your application so that it shows more statistics about the gathered feedback: the total number of collected feedback, the average score (good: 1, neutral: 0, bad: -1) and the percentage of positive feedback.

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -2,19 +2,28 @@ import { useState } from 'react'
 
 const App = () => {
 
-  const handleClickBad = () => {
-    setBad(currentValue => currentValue + 1)
+  const handleClickGood = () => {
+    setAverage(currentValue => currentValue + 1)
+    setGood(currentValue => currentValue + 1)
+    setAll(currentValue => currentValue + 1)
+    
   }
   const handleClickNeutral = () => {
     setNeutral(currentValue => currentValue + 1)
+    setAll(currentValue => currentValue + 1)
   }
-  const handleClickGood = () => {
-    setGood(good => good + 1)
+  const handleClickBad = () => {
+    setAverage(currentValue => currentValue - 1)
+    setBad(currentValue => currentValue + 1)
+    setAll(currentValue => currentValue + 1)
   }
+
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
+  const [average, setAverage] = useState(0)
 
   return (
     <div>
@@ -23,9 +32,12 @@ const App = () => {
       <button onClick={handleClickNeutral}>neutral</button>
       <button onClick={handleClickBad}>bad</button>
       <h1>statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
+      <p style={{margin:0}}>good {good}</p>
+      <p style={{margin:0}}>neutral {neutral}</p>
+      <p style={{margin:0}}>bad {bad}</p>
+      <p style={{margin:0}}>all {all}</p>
+      <p style={{margin:0}}>average {(all === 0 ? 0 : average / all)}</p>
+      <p style={{margin:0}}>positive {(all === 0 ? 0 :(good*100)/all)}%</p>
     </div>
   )
 }
