cf74a7dfb30057a70da1b01f1ce39a476bcace7e
Miguel Sequera <msequera197@gmail.com>
2024-11-01 18:35:50 -0400

    Exercise 1.8 Unicafe step 3:Refactor your application so that displaying the statistics is extracted into its own Statistics component. The state of the application should remain in the App root component.

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,6 +1,27 @@
 import { useState } from 'react'
 
+const Statistics = (props) => {
+  return (
+    <div>
+      <h1>statistics</h1>
+      <p style={{margin:0}}>good {props.good}</p>
+      <p style={{margin:0}}>neutral {props.neutral}</p>
+      <p style={{margin:0}}>bad {props.bad}</p>
+      <p style={{margin:0}}>all {props.all}</p>
+      <p style={{margin:0}}>average {(props.all === 0 ? 0 : props.average / props.all)}</p>
+      <p style={{margin:0}}>positive {(props.all === 0 ? 0 :(props.good*100)/props.all)}%</p>
+    </div>
+  )
+}
+
 const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
+  const [average, setAverage] = useState(0)
+
 
   const handleClickGood = () => {
     setAverage(currentValue => currentValue + 1)
@@ -18,27 +39,20 @@ const App = () => {
     setAll(currentValue => currentValue + 1)
   }
 
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-  const [all, setAll] = useState(0)
-  const [average, setAverage] = useState(0)
-
   return (
-    <div>
+    <>
       <h1>give feedback</h1>
       <button onClick={handleClickGood}>good</button>
       <button onClick={handleClickNeutral}>neutral</button>
       <button onClick={handleClickBad}>bad</button>
-      <h1>statistics</h1>
-      <p style={{margin:0}}>good {good}</p>
-      <p style={{margin:0}}>neutral {neutral}</p>
-      <p style={{margin:0}}>bad {bad}</p>
-      <p style={{margin:0}}>all {all}</p>
-      <p style={{margin:0}}>average {(all === 0 ? 0 : average / all)}</p>
-      <p style={{margin:0}}>positive {(all === 0 ? 0 :(good*100)/all)}%</p>
-    </div>
+      <Statistics 
+        good = {good}
+        neutral = {neutral}
+        bad = {bad}
+        all = {all}
+        average = {average}
+      />
+    </>
   )
 }
 
